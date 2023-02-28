14e27dad227866a2d089a6e86a03717e2fdb5992
banrj <eh.ehr@bk.ru>
2023-02-28 03:39:57 +0200

    add Iclone and Icomparable

--- 4/Note.cs
+++ 4/Note.cs
@@ -1,12 +1,48 @@
 ﻿using System;
+using System.Threading;
+using System.Xml.Linq;
+
 namespace _4
 {
-	public class Note
+	public class Note<T> : ICloneable, IComparable
 	{
-		public Note()
+		public string UserName { get; private set; }
+		public T Number { get; private set; }
+		public int[] Birthday { get; private set; }
+
+
+		public Note(string username, T number, int[] birthday)
 		{
-				
+			UserName = username;
+			Number = number;
+			Birthday = birthday;
 		}
-	}
+
+		public object Clone()
+		{
+			return new Note<T>(UserName = this.UserName, Number = this.Number,
+								Birthday = this.Birthday);
+		}
+
+		public int CompareTo(object? obj)
+		{
+            if (obj is Note<T> note)
+            {
+				return string.Compare(this.UserName, note?.UserName);
+            }
+            else
+                throw new Exception("Невозможно сравнить");
+
+
+        }
+
+        public override string? ToString()
+        {
+            return $"Имя: {UserName} Number: {Number} " +
+                $"Birthday: {Birthday}";
+        }
+
+    }
 }
+
 
--- 4/Program.cs
+++ 4/Program.cs
@@ -3,7 +3,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Note<int>[] notes = new Note<int>[6]
+        {
+             new Note<int>("Даниил",19646392, new int[] {22, 3, 2004 }),
+             new Note<int>("Алеша",1312988, new int[] {21, 3, 2004 }),
+             new Note<int>("Алена",311932, new int[] {02, 6, 2000 }),
+             new Note<int>("Вова",12341932, new int[] {22, 12, 2007 }),
+             new Note<int>("Зоя",541999, new int[] {31, 01, 1999 }),
+             new Note<int>("дима",1908977, new int[] {24, 5, 1987 })
+        };
+
+        Array.Sort(notes);
+        foreach (var item in notes)
+        {
+            Console.WriteLine(item);
+        }
+        Console.WriteLine();
+        //Array.Sort(notes, new SortByAmmo());
+        //foreach (var item in notes)
+        //{
+        //    Console.WriteLine(item);
+        //}
+
     }
 }
 
